7225cbc6bb7266eab76508fb7b6e125547be6edd
cleitondemelod249 <cleitondemelo@live.com>
2020-04-04 15:17:22 -0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PWA With <code>React</code> to deploy on github.io.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/cleitondemelod249/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github: cleitondemelod29
         </a>
       </header>
     </div>
